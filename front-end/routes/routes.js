4a9c79510389f2c4a55b9966619eb982e01ee5f8
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2015-10-18 00:52:13 +1000

    Frontend routing started, Working partially ; JESUS TAKE THE WHEEL

--- front-end/routes/routes.js
+++ front-end/routes/routes.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var Q = require('q');
+
 
 var keys = require('./../config/api-keys.json');
+var backendRoutes = require('./../config/backend-routes.json');
 
 var router = express.Router();
 
-
+/* ROUTES SECTION */
 
 router.get('/',
     /**
@@ -58,7 +61,7 @@ router.get('/watch',
 
         // If the last character is a comma, removes it
         cams = cams.slice(-1) == "," ? cams.substr(0, cams.length - 1) : cams;
-        console.log(cams);
+
 
 
         /*
@@ -69,16 +72,44 @@ router.get('/watch',
             headers: {
                 'content-type': 'application/x-www-form-urlencoded'
             },
-            url: 'http://nyc-balancer-1920328868.ap-southeast-2.elb.amazonaws.com/cars',
+            url: backendRoutes.cars_detection,
             form: {
                 camIds: cams,
                 api: keys.backend_server
             }
         }, function (err, response, body) {
-            res.send(body);
+
+            body = JSON.parse(body);
+
+
+            var targetCams = [];
+
+
+            // Creating a list of cams to be
+            for (cam in body) {
+                targetCams.push(cam);
+            }
+
+            getCamInfo(targetCams)
+                .then(function (camsInfo) {
+
+                    // Adding the number of cars from the backend to the info array
+                    for (cam in camsInfo) {
+                        camsInfo[cam]['num_cars'] = body[cam].valueOf();
+                    }
+                    //Updating the data to be rendered
+                    console.log(camsInfo); //Outputs normally
+
+                    res.send(JSON.stringify(camsInfo)); //Simple response without view.
+                })
+                //Error Handling
+                .catch(function (err) {
+                    res.status(500).send(err);
+                });
         });
 
     });
+
 
 router.post('/watch',
     /**
@@ -90,6 +121,58 @@ router.post('/watch',
      */
     function (req, res) {
         res.send("404");
-    });
+    }
+);
+
+/* HELPER FUNCTIONS SECTION */
+
+/**
+ * Gets the cam(s) information from the JSON file and returns it in the form of a promise.
+ * @param   {Array}   camIds The ID or IDs of the cams which data will be returned
+ * @returns {Promise} A promise holding the Cam(s) information or an error message.
+ */
+function getCamInfo(camIds) {
+
+    var deferred = Q.defer();
+
+    if (!typeof (camIds) == "object" || camIds.length < 1) {
+        return deferred.reject("Invalid Object");
+    }
+
+    var finalCamInfoObj = [];
+
+    getCamsJson()
+        .then(function (camsJSON) {
+            camIds.map(function (cam) {
+                finalCamInfoObj[cam] = camsJSON[cam];
+            });
+
+            deferred.resolve(finalCamInfoObj);
+        })
+        .catch(function (e) {
+            deferred.reject(e);
+        });
+
+    return deferred.promise;
+}
+
+/**
+ * Requests a JSON with camera information about the cameras,
+ * and returns it as a promise (or rejects with the error message)
+ *
+ * @returns {Promise} The promise holding the data or an error message.
+ */
+function getCamsJson() {
+    var deferred = Q.defer();
+    request.get(backendRoutes.cams_json + keys.backend_server,
+        function (err, resp, data) {
+            if (!err && resp.statusCode == 200) {
+                deferred.resolve(JSON.parse(data));
+            }
+            deferred.reject(err);
+        });
+    return deferred.promise;
+}
+
 
 module.exports = router;
